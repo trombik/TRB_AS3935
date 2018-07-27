a146a6b2c013858221805f440f044778f2879812
Tomoyuki Sakurai <y@trombik.org>
2018-07-27 09:01:12 +0900

    bugfix: hide unnecessary files while building, build esp-idf example
    the latest development version of platformio is required, as
    `src_filter` breaks the build.

--- prebuild.py
+++ prebuild.py
@@ -1,1 +1,12 @@
-# nothing to do yet
+Import("env")
+src_filter = ["+<TRB_AS3935.h>"]
+env.Replace(SRC_FILTER=src_filter)
+
+build_flags = env.ParseFlags(env['BUILD_FLAGS'])
+cppdefines = build_flags.get("CPPDEFINES")
+
+if "TRB_AS3935_ESP_IDF" in cppdefines:
+    env.Append(SRC_FILTER=["+<TRB_AS3935.c>"])
+if "TRB_AS3935_ARDUINO_WIRE" in cppdefines:
+    env.Append(SRC_FILTER=["+<TRB_AS3935.cpp>"])
+#print env.get("SRC_FILTER")
